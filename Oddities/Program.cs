b000c900bab56e4b72a9f826a785032b2173a808
Alex Telon <alex.telon@gmail.com>
2017-10-14 13:01:30 +0200

    Solved Oddities

---
+++ Oddities/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kattis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var results = new List<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                string text = "is odd";
+                if (x % 2 == 0) text = "is even";
+
+                results.Add(x + " " + text);
+            }
+
+
+            Console.WriteLine(String.Join("\n", results));
+        }
+    }
+}
