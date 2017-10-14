456471537eb91d37705d7db4df8a48a2711fa8f2
Alex Telon <alex.telon@gmail.com>
2017-10-14 12:52:58 +0200

    solved pot and planina

---
+++ planina/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kattis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int side = 2;
+            for (int i = 1; i <= n; i++)
+            {
+                side += side - 1;
+            }
+            int sum = side * side;
+
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ pot/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kattis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sum = 0;
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string p = Console.ReadLine();
+                int number = int.Parse(p.Substring(0, p.Length - 1));
+                int power = (int)Char.GetNumericValue(p[p.Length - 1]);
+
+                sum += Math.Pow(number, power);
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
