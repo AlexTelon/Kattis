638cffc357ea29319d8caa80d4419b6b79a616f4
Alex Telon <alex.telon@gmail.com>
2017-10-14 12:56:51 +0200

    Solved Pet

---
+++ pet/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kattis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> results = new List<int>();
+            for (int i = 0; i < 5; i++)
+            {
+                List<int> tokens = Console.ReadLine().Split().Select(int.Parse).ToList();
+                results.Add(tokens.Sum());
+            }
+
+            int max = results.Max();
+            int winner = results.FindIndex(x => x == max);
+
+            // winner represented from index 1
+            Console.WriteLine(++winner + " " + max);
+        }
+    }
+}
