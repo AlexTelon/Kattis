467e99586d57b6fc33e0565d5fa53b248eda601e
Alex Telon <alex.telon@gmail.com>
2018-02-06 21:15:18 +0100

    helpaphd

---
+++ helpaphd/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace Kattis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < lines; i++)
+            {
+                var line = Console.ReadLine();
+                
+                if (line == "P=NP")
+                {
+                    Console.WriteLine("skipped");
+                    continue;
+                }
+
+                var arguments = line.Split('+').Select(int.Parse).ToList();
+
+                Console.WriteLine(arguments[0] + arguments[1]);
+            }
+        }
+    }
+}
