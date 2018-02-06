0eeaebc2a5bac9b85044acc68e56f68947c92ce9
Alex Telon <alex.telon@gmail.com>
2018-02-06 21:09:56 +0100

    railroad2

---
+++ railroad2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace Kattis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            if (input[1] % 2 == 0)
+            {
+                Console.WriteLine("possible");
+            } else
+            {
+                Console.WriteLine("impossible");
+            }
+        }
+    }
+}
