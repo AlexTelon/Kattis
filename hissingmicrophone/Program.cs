ad2c89330a7ad6fed1109527dc99af4c8fe5cc9f
Alex Telon <alex.telon@gmail.com>
2018-02-06 20:15:54 +0100

    hissingmicrophone

---
+++ hissingmicrophone/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Kattis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+
+                if (input[i] == 's' && input[i + 1] == 's')
+                {
+                    Console.WriteLine("hiss");
+                    return;
+                } 
+
+            }
+            Console.WriteLine("no hiss");
+        }
+    }
+}
