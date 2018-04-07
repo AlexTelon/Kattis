23ebecd46fe0dd0a4b28f3714edd626b44a5f2d8
Alex Telon <alex.telon@gmail.com>
2018-04-07 21:51:14 +0200

    nodup

---
+++ nodup/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace Kattis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var words = Console.ReadLine().Split().ToList();
+
+            // check for dups
+            var duplicates = (words.Count() != words.Distinct().Count());
+
+            if (!duplicates)
+            {
+                Console.WriteLine("yes");
+            } else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
