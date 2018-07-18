3d03dd1fc5c6fc554e88a78acd0870aed6e399fa
Alex Telon <alex.telon@gmail.com>
2018-07-18 21:00:20 +0200

    quickbrownfox c#

---
+++ quickbrownfox/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace Kattis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                var inputstring = Console.ReadLine();
+
+                string missingLetters = MissingLettersToBePangram(inputstring);
+
+                if (string.IsNullOrEmpty(missingLetters))
+                {
+                    Console.WriteLine("pangram");
+                } else
+                {
+                    Console.WriteLine("missing " + missingLetters);
+                }
+            }
+        }
+
+        private static string MissingLettersToBePangram(string inputstring)
+        {
+            string missing = "";
+            foreach (var alphabet in "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToLower())
+            {
+                if (!inputstring.ToLower().Contains(alphabet))
+                {
+                    missing += alphabet;
+                }
+            }
+            return missing;
+        }
+    }
+}
