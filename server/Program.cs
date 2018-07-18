d9ad106cd204eb03b1c955c66e3eb89b9b974922
Alex Telon <alex.telon@gmail.com>
2018-07-18 20:43:03 +0200

    server c#

---
+++ server/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Kattis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> tokens = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int time = tokens[1];
+
+            List<int> tasks = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            int sum = 0;
+            int count = 0;
+            foreach (var task in tasks)
+            {
+                sum += task;
+                if (sum > time) break;
+                count++;
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
