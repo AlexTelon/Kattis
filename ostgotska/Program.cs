b0e943a0d0770d4dec84d541e82e97dd35dc5981
Alex Telon <alex.telon@gmail.com>
2018-07-22 23:27:01 +0200

    ostgotska c#
    When you read the question wrong life get difficult at first!

---
+++ ostgotska/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Kattis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+
+            var words = input.Trim().Split().Where(s => s.Length > 0);
+
+            int ostgotskaWords = 0;
+            foreach (var word in words)
+            {
+                int count = new Regex(Regex.Escape("ae")).Matches(word).Count;
+                if (count > 0)
+                {
+                    ostgotskaWords++;
+                }
+            }
+
+            int wordCount = words.Count();
+            if (wordCount == 0 || (double)ostgotskaWords / wordCount >= 0.4)
+            {
+                Console.WriteLine("dae ae ju traeligt va");
+            }
+            else
+            {
+                Console.WriteLine("haer talar vi rikssvenska");
+            }
+        }
+    }
+}
