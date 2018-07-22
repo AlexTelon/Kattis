a1e912a7e9ce7269a48877d5f1326d7ad3ead0df
Alex Telon <alex.telon@gmail.com>
2018-07-22 21:27:29 +0200

    justaminute c#

---
+++ justaminute/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace Kattis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            double reportedMinutes = 0;
+            double realSecondsPassed = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                var tokens = Console.ReadLine().Split().Select(int.Parse).ToList();
+                var M = tokens[0];
+                var seconds = tokens[1];
+
+                reportedMinutes += M;
+                realSecondsPassed += seconds;
+            }
+
+            double realMinutesPassed = (realSecondsPassed / 60);
+
+            var averageRealMinuteLength = realMinutesPassed / reportedMinutes;
+
+            if (averageRealMinuteLength <= 1)
+            {
+                Console.WriteLine("measurement error");
+                return;
+            }
+            Console.WriteLine(averageRealMinuteLength);
+        }
+    }
+}
