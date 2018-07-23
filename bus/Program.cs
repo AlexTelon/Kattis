34a0f544dbe2fca1a529da989feb8387ab24deb4
Alex Telon <alex.telon@gmail.com>
2018-07-23 18:18:29 +0200

    bus c#

---
+++ bus/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Kattis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                int stops = int.Parse(Console.ReadLine());
+
+                var passangers = NrOfPassangersAtStart(stops);
+                Console.WriteLine(passangers);
+            }
+        }
+
+        private static double NrOfPassangersAtStart(int k)
+        {
+            double passangers = 1;
+            for (int i = 0; i < k - 1; i++)
+            {
+                passangers = 2 * (passangers + 0.5);
+            }
+
+            return passangers;
+        }
+    }
+}
